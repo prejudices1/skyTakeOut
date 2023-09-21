d4788cf68edf335adf1861c3b81f440d9542576b
prejudices1 <zhoubian@bupt.edu.cn>
2023-09-22 00:11:10 +0800

    新增员工分页代码

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -86,12 +86,10 @@ public class EmployeeController {
         employeeService.save(employeeDTO);
         return Result.success();
     }
-    @GetMapping
+    @GetMapping("/page")
     @ApiOperation("员工分页查询")
     public Result<PageResult> page(EmployeePageQueryDTO employeePageQueryDTO){
-//        PageHelper.startPage(employeePageQueryDTO.getPage(),employeePageQueryDTO.getPageSize());
-//        Page<Employee> page=employeeMapper.pageQuery(employeePageQueryDTO);
-        log.info("员工分页查询");
+        log.info("员工分页查询"+employeePageQueryDTO);
         PageResult pageResult=employeeService.pageQuery(employeePageQueryDTO);
         return Result.success(pageResult);
     }
